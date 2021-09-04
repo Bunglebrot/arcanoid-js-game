6c26e1f2f8580bf56579b35bfcb66cfed26a0662
Khitrov Nikolai <deliriumbred@yandex.ru>
2021-09-04 15:52:00 +0300

    Refactoring methods of the game object. Add functional of it

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 'use strict';
 
 const game = {
-  start: function() {
+  ctx: null,
+  sprites: {
+    background: null,
+    ball: null,
+    platform: null,
+  },
+  init() {
     this.ctx = document.getElementById('my-canvas').getContext('2d');
-    let background = new Image();
-    background.src = './img/background.png';
+  },
+  preload(callback) {
+    let loaded = 0;
+    let required = Object.keys(this.sprites).length;
+
+    for (let item in this.sprites) {
+      this.sprites[item] = new Image();
+      this.sprites[item].src = `./img/${item}.png`;
+      this.sprites[item].addEventListener('load', () => {
+        ++loaded;
+        if (loaded >= required) {
+          callback();
+        }
+      });
+    }
+  },
+  run() {
     window.requestAnimationFrame(() => {
-      this.ctx.drawImage(background, 0, 0);
+      this.render();
     });
+  },
+  render() {
+    this.ctx.drawImage(this.sprites.background, 0, 0);
+    this.ctx.drawImage(this.sprites.ball, 0, 0);
+    this.ctx.drawImage(this.sprites.platform, 280, 330);
+  },
+  start() {
+    this.init();
+    this.preload( () => this.run() );
   },
 };
 
