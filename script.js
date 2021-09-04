47f18da7f4df60644291cfda0fd579e80fffd1f4
Khitrov Nikolai <deliriumbred@yandex.ru>
2021-09-04 14:58:43 +0300

    Add a script.js file. Create a main object a game and setting a field start of object the game

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const game = {
+  start: function() {
+    this.ctx = document.getElementById('my-canvas').getContext('2d');
+    let background = new Image();
+    background.src = './img/background.png';
+    window.requestAnimationFrame(() => {
+      this.ctx.drawImage(background, 0, 0);
+    });
+  },
+};
+
+window.addEventListener('load', () => {
+  game.start();
+});
