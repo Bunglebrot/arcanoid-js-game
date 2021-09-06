d56a10c6a3cde0e43cce0f213a1cc0845822bf94
Khitrov Nikolai <deliriumbred@yandex.ru>
2021-09-06 14:19:31 +0300

    Add a logic of render blocks in the game. refactoring function onImageLoad

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ const game = {
   ctx: null,
   ball: null,
   platform: null,
+  blocks: [],
+  rows: 4,
+  cols: 8,
   sprites: {
     background: null,
     ball: null,
     platform: null,
+    block: null
   },
   init() {
     this.ctx = document.getElementById('my-canvas').getContext('2d');
@@ -19,12 +23,26 @@ const game = {
     for (let item in this.sprites) {
       this.sprites[item] = new Image();
       this.sprites[item].src = `./img/${item}.png`;
-      this.sprites[item].addEventListener('load', () => {
+      let onImageLoad = () => {
         ++loaded;
         if (loaded >= required) {
           callback();
         }
-      });
+      };
+
+      this.sprites[item].addEventListener('load', onImageLoad);
+    }
+  },
+  create() {
+    for (let row = 0; row < this.rows; row += 1) {
+      for (let col = 0; col < this.cols; col += 1) {
+        this.blocks.push(
+          {
+            x: 64 * col + 65,
+            y: 24 * row + 35
+          }
+        )
+      }
     }
   },
   run() {
@@ -36,10 +54,19 @@ const game = {
     this.ctx.drawImage(this.sprites.background, 0, 0);
     this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
     this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+    this.renderBlocks();
+  },
+  renderBlocks() {
+    for (let block of this.blocks) {
+      this.ctx.drawImage(this.sprites.block, block.x, block.y);
+    }
   },
   start() {
     this.init();
-    this.preload( () => this.run() );
+    this.preload(() => {
+      this.create();
+      this.run();
+    });
   },
 };
 
