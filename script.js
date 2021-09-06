61592b583104b2be7bb9c9dfa3166c3aa35e8512
Khitrov Nikolai <deliriumbred@yandex.ru>
2021-09-06 13:50:07 +0300

    Create properties a ball and a platform of the game object. Configure them

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const game = {
   ctx: null,
+  ball: null,
+  platform: null,
   sprites: {
     background: null,
     ball: null,
@@ -32,13 +34,25 @@ const game = {
   },
   render() {
     this.ctx.drawImage(this.sprites.background, 0, 0);
-    this.ctx.drawImage(this.sprites.ball, 0, 0);
-    this.ctx.drawImage(this.sprites.platform, 280, 330);
+    this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+    this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
   },
   start() {
     this.init();
     this.preload( () => this.run() );
   },
+};
+
+game.ball = {
+  x: 320,
+  y: 280,
+  width: 20,
+  height: 20,
+};
+
+game.platform = {
+  x: 280,
+  y: 300,
 };
 
 window.addEventListener('load', () => {
